6c46a49ec095b913b4238bd4c173abdb1f1e6968
kidus <kidussolomon2004@gmail.com>
2020-10-18 11:47:08 +0300

    code refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:akile_attendance_system/pages/login.dart';
+import 'package:akile_attendance_system/pages/home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(AkileAttendanceApp());
 }
 
-class MyApp extends StatelessWidget {
+class AkileAttendanceApp extends StatelessWidget {
+  // entry Widget for the app, with all routes defined
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Akile Attendance Management System',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-
-    final emailField = TextField(
-      obscureText: false,
-      decoration: InputDecoration(
-        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        hintText: "User Name",
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-      ),
-    );
-    final passwordField = TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        hintText: "Password",
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-      ),
-    );
-
-    final loginButton = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(15.0),
-      color: Color(0xff01A0C7),
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
-        child: Text("Login",
-          textAlign: TextAlign.center,
-        ),
-      ),
-    );
-
-    return Scaffold(
-      body: Center(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: EdgeInsets.all(36.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 155.0,
-                  child: Image.asset(
-                    "assets/logo.jpg",
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                SizedBox(height: 45.0),
-                emailField,
-                SizedBox(height: 25.0),
-                passwordField,
-                SizedBox(height: 35.0),
-                loginButton,
-                SizedBox(height: 15.0),
-              ],
-            ),
-          ),
-        ),
-      ),
+      routes: <String, WidgetBuilder>{
+        'login': (BuildContext context) => Login(),
+        'home': (BuildContext context) => Home()
+      },
+      initialRoute: 'login',
     );
   }
 }
