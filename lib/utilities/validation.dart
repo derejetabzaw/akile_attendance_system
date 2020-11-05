ce8374c0473c9b0f3a968f637a0885d8924ead78
kidus <kidussolomon2004@gmail.com>
2020-11-05 04:44:19 +0300

    helpers methods for validation and getting size

---
+++ lib/utilities/validation.dart
@@ -0,0 +1,27 @@
+// Validate any string field including staffId
+validateStringField(String value) {
+  if (value.isEmpty||value==null) {
+    return "This field is required! ";
+  }
+  // This is just a regular expression for email addresses
+  String p = "[a-zA-Z]{1,256}";
+  RegExp regExp = new RegExp(p);
+
+  if (regExp.hasMatch(value)) {
+    return "";
+  }
+  return 'Value is not valid';
+}
+
+// Validate login password
+validatePassword(String value) {
+  if (value.isEmpty) {
+    return "Enter Password field";
+  }
+  if (value.length < 8) {
+    return 'Your password is too weak';
+  }
+
+  return "";
+}
+
