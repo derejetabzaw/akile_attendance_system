6d3de9bde596c2025f9b4a57f4c8b8d63736b1c6
kidus <kidussolomon2004@gmail.com>
2020-11-10 16:03:09 +0300

    switch to depolyed backend api

--- lib/api/endpoints.dart
+++ lib/api/endpoints.dart
@@ -1,4 +1,4 @@
 class API {
-  static const String BASE_API = "https://97e271a8ba58.ngrok.io/api/v1/";
+  static const String BASE_API = "https://akile-attendance-system.herokuapp.com/api/v1/";
   static const String LOGIN_API = BASE_API + "auth/login";
 }
