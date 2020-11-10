86563417e74afbcc5589651d12c38e96e0ff2747
kidus <kidussolomon2004@gmail.com>
2020-11-10 14:32:45 +0300

    modified signin page and added functionality to login

---
+++ lib/pages/signin.dart
@@ -0,0 +1,246 @@
+import 'package:flutter/material.dart';
+import 'package:akile_attendance_system/api/auth.dart';
+import 'package:akile_attendance_system/constants/colors.dart';
+import 'package:akile_attendance_system/constants/constant.dart';
+import 'package:akile_attendance_system/pages/sharedPreference/sharedPreference.dart';
+import 'package:akile_attendance_system/pages/home.dart';
+import 'package:akile_attendance_system/pages/logo/logo.dart';
+import 'package:akile_attendance_system/pages/slider/slider.dart';
+import 'package:akile_attendance_system/pages/widgets/circularProgressBar.dart';
+import 'package:akile_attendance_system/pages/widgets/clip_shape.dart';
+import 'package:akile_attendance_system/state/appState.dart';
+import 'package:akile_attendance_system/utilities/validation.dart';
+import 'package:provider/provider.dart';
+
+class SignInPage extends StatefulWidget {
+  final String title;
+  SignInPage({this.title});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<SignInPage> {
+  bool isLoading = false;
+  bool showError = false;
+  bool showBackendError = false;
+  TextEditingController staffIdController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool _secureText = true;
+  bool isDark = false;
+  String staffIdError = "", passwordError = "";
+
+
+  @override
+  void dispose() {
+    staffIdController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  showHide() {
+    setState(() {
+      _secureText = !_secureText;
+    });
+  }
+
+  submitForm() async {
+    setState(() {
+      showError = true;
+      staffIdError = validateStringField(staffIdController.text);
+      passwordError = validatePassword(passwordController.text);
+    });
+
+    if (staffIdError.isEmpty && passwordError.isEmpty && showError) {
+      Provider.of<Auth>(context, listen: false).setLoadingStateFun(true);
+      var _loginModel = loginApi(
+        staffId: staffIdController.text,
+        password: passwordController.text,
+        context: context,
+      );
+      _loginModel.then((value) async {
+        savePref(
+          accessToken: value.accessToken,
+        );
+        Provider.of<Auth>(context, listen: false).setLoadingStateFun(false);
+        Navigator.push(context, SlideLeftRoute(
+            page: Home()
+        ));
+      });
+
+      _loginModel.catchError((value) async {
+        Provider.of<Auth>(context, listen: false).setHasErrorFun(value);
+        Provider.of<Auth>(context, listen: false).setLoadingStateFun(false);
+      });
+    }
+    else {
+      Provider.of<Auth>(context, listen: false).setHasErrorFun("");
+    }
+  }
+  submitButton() {
+    return
+      Row(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child:  RawMaterialButton(
+            onPressed: () {
+              submitForm();
+            },
+            child: new Icon(
+              Icons.arrow_forward,
+              color: TRIAL_COLOR,
+              size: 35.0,
+            ),
+            shape: new CircleBorder(),
+            elevation: 2.0,
+            fillColor:PRIMARY_COLOR,
+            padding: const EdgeInsets.all(15.0),
+          ),
+        )
+      ],
+    );
+  }
+
+
+  staffIdTextFormField() {
+    return Column(
+      children: <Widget>[
+        Material(
+          borderRadius: BorderRadius.circular(30.0),
+          elevation: 12,
+          child: TextFormField(
+            controller: staffIdController,
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.email,
+                size: 20,
+                color: PRIMARY_COLOR,
+              ),
+              hintText: "Staff Id",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide.none),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        (showError == true && staffIdError.isNotEmpty) ? 
+        Text(staffIdError, style: TextStyle(color: Colors.red)) : 
+        Container(),
+      ],
+    );
+  }
+
+  passwordTextFormField() {
+    return Column(
+      children: <Widget>[
+        Material(
+          borderRadius: BorderRadius.circular(30.0),
+          elevation: 11,
+          child: TextFormField(
+            obscureText: _secureText,
+            controller: passwordController,
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.lock, size: 20, color: PRIMARY_COLOR),
+              hintText: "Password",
+              suffixIcon: IconButton(
+                onPressed: showHide,
+                icon:
+                Icon(_secureText ? Icons.visibility_off : Icons.visibility),
+              ),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide.none),
+            ),
+          ),
+        ),
+        (showError == true && passwordError.isNotEmpty) ? 
+        Text(passwordError, style: TextStyle(color: Colors.red)) : 
+        Container(),
+      ],
+    );
+  }
+  
+  headerTextRow() {
+    return Container(
+      margin: EdgeInsets.only(left: 15.0),
+      child: Row(
+        children: <Widget>[
+          Text(
+            "Login",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+
+          ),
+        ],
+      ),
+    );
+  }
+
+  forms() {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child:Column(
+        children: <Widget>[
+
+          SizedBox(height: 15,),
+          logo(context),
+          SizedBox(
+            height: 15,
+          ),
+          headerTextRow(),
+          SizedBox(
+            height: 15,
+          ),
+          staffIdTextFormField(),
+          SizedBox(
+            height: 15,
+          ),
+          passwordTextFormField(),
+          SizedBox(
+            height: 15,
+          )
+          ,
+      Consumer<Auth>(
+                    builder: (BuildContext context, Auth value, Widget child) =>
+                    value.getHasErrorFun().toString().isNotEmpty == true ?
+                    Text(value.getHasErrorFun(),style: TextStyle(color: Colors.red)) :
+                    Container(),
+                  ),
+          SizedBox(
+            height: 20,
+          ),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Provider.of<Auth>(context).getIsLoadingFun() == true
+          ? circularIndicator(context: context):
+          SingleChildScrollView(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Provider.of<AppState>(context).getTheme()==Constant.lightTheme?
+              clipShape(context)
+              :SizedBox(height: 100,),
+              Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: forms(),
+              ),
+            ],
+          )
+        ),
+      );
+  }
+}
+
+
