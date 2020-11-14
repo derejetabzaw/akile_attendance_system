8f75860d3e695907f3a3d2e324af7e541a1bf759
kidus <kidussolomon2004@gmail.com>
2020-11-14 16:42:35 +0300

    remove regex validation

--- lib/utilities/validation.dart
+++ lib/utilities/validation.dart
@@ -3,14 +3,7 @@ validateStringField(String value) {
   if (value.isEmpty||value==null) {
     return "This field is required! ";
   }
-  // This is just a regular expression for email addresses
-  String p = "[a-zA-Z]{1,256}";
-  RegExp regExp = new RegExp(p);
-
-  if (regExp.hasMatch(value)) {
-    return "";
-  }
-  return 'Value is not valid';
+  return "";
 }
 
 // Validate login password
